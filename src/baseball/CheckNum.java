3eb7084c911b8a0aea2b7f41e84d30133170435b
github eunbiro <github ebee91@naver.com>
2022-12-12 16:08:11 +0900

    up

--- src/baseball/CheckNum.java
+++ src/baseball/CheckNum.java
@@ -1,5 +1,67 @@
 package baseball;
 
-public class CheckNum {
+import java.util.Scanner;
 
+public class CheckNum {
+	RandomNum randomNum = new RandomNum();
+	
+	
+	int[] inputNum = new int [4];
+	int[] ramNum = randomNum.getRandomNum();
+	int stkNum = 0;
+	int ballNum = 0;
+
+	Scanner scanner = new Scanner(System.in);
+	
+	public void getCheckNum() {
+		System.out.println("======================");
+		System.out.println(" 4자리 숫자를 입력해 주세요.");
+		System.out.println("======================");
+		System.out.print(">");
+		while (true) {
+			String scan = scanner.next();
+			
+			if (scan.length() == 4) {
+				
+				for (int i = 0; i < 4; i++) {
+					char charScan = scan.charAt(i);
+					inputNum[i] = charScan - '0';
+					System.out.print(inputNum[i] + " ");
+					
+				}
+			} else {
+				System.out.println("********************");
+				System.out.println(" 4자리 숫자만 입력해주세요.");
+				System.out.println("********************");
+				continue;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				if (inputNum[i] == ramNum[i]) {
+					stkNum += 1;
+				} else {
+					for (int j = 0; j < 4; j++) {
+						if (inputNum[i] == ramNum[j]) {
+							ballNum += 1;
+						}
+					}
+				}
+			}
+			System.out.println("\n>> " + stkNum + " 스트라이크 | " + ballNum + " 볼");
+			
+			if (stkNum == 4) {
+				System.out.println("====================");
+				System.out.println(" 정답입니다!");
+				break;
+			} else {
+				System.out.println("====================");
+				System.out.println("   다시 시도해주세요!");
+				System.out.println("====================");
+				System.out.print(">");
+			}
+			
+			stkNum = 0;
+			ballNum = 0;
+		}
+	};
 }
--- src/baseball/GameEx.java
+++ src/baseball/GameEx.java
@@ -1,24 +1,13 @@
 package baseball;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class GameEx {
 
 	public static void main(String[] args) {
-		RandomNum randomNum = new RandomNum();
+		CheckNum checkNum = new CheckNum();
 		
-		System.out.println(Arrays.toString(randomNum.getRandomNum()));
-		
-		
-		int[] chkNum = {4, 3, 7, 9};
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		String tmp = scanner.nextLine();
-		int inputNum = Integer.parseInt(tmp);
+		checkNum.getCheckNum();
 		
 		
-
+		
 	}
 }
