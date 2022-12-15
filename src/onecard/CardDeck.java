5c4e447857dfa8ad487395abdd2c548140db56dc
github eunbiro <github ebee91@naver.com>
2022-12-16 00:17:38 +0900

    up

--- src/onecard/CardDeck.java
+++ src/onecard/CardDeck.java
@@ -1,6 +1,9 @@
 package onecard;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class CardDeck {
 	
@@ -9,39 +12,64 @@ public class CardDeck {
 	static final int CLOVER = 2;
 	static final int HEART = 3;
 	
-	ArrayList<Card> cards = new ArrayList<>();
+	List<Card> cards = new ArrayList<>();
 	
 	public CardDeck() {
-		for (int i = 0; i < 13; i++) {
-			
+		for (int type = 0; type < 4; type++) {
+			for (int number = 0; number < 13; number++) {
+				cards.add(new Card(type, number));
+			}
 		}
 	}
 	
-	public void createCard() {
-		for (int i = 0; i < 13; i++) {
-			
+	public CardDeck(String jorker) {
+		for (int type = 0; type < 4; type++) {
+			for (int number = 0; number < 13; number++) {
+				cards.add(new Card(type, number));
+			}
 		}
+		// 조커 타입 4, 숫자 0
+		cards.add(new Card(4, 0));
+	}
+	
+	public void createCard() {
 		
+	}
+	
+	public void shuffle() {
+		
+		Collections.shuffle(cards);
 	}
 	
 	public static void main(String[] args) {
 		CardDeck cDeck = new CardDeck();
-		cDeck.createCard();
+		cDeck.shuffle();
 		
 		for (Card card : cDeck.cards) {
 			
-			if (card.getNumber() == 1) {
+			if (card.getNumber()+1 == 1) {
 				System.out.print("A");
-			} else if (card.getNumber() == 11) {
+			} else if (card.getNumber()+1 == 11) {
 				System.out.print("J");
-			} else if (card.getNumber() == 12) {
+			} else if (card.getNumber()+1 == 12) {
 				System.out.print("Q");
-			} else if (card.getNumber() == 13) {
+			} else if (card.getNumber()+1 == 13) {
 				System.out.print("K");
 			} else {
 				System.out.print(card.getNumber());
 			}
-			System.out.println(card.getType());
+			
+			if (card.getType() == 0) {
+				System.out.println("♠");
+			} else if (card.getType() == 1) {
+				System.out.println("◇");
+			} else if (card.getType() == 2) {
+				System.out.println("♡");
+			} else if (card.getType() == 3) {
+				System.out.println("♣");
+			} else if (card.getType() == 4) {
+				System.out.println("★");
+			}
 		}
 	}
 
